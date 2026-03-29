c4c41bbc13d1b6ff1c25433037493f8f2fc4029c
raghavbhise <bhiseraghav@gmail.com>
2026-03-29 19:07:38 +0530

    new file

---
+++ service/__init__.py
@@ -0,0 +1,12 @@
+from flask import Flask
+from flask_talisman import Talisman
+
+def create_app():
+app = Flask(**name**)
+
+```
+# Enable security headers using Talisman
+Talisman(app)
+
+return app
+```
